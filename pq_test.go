38ebb3365f62b33582b98db3f7aff758b8ddfedd
Khalid Lafi <lafi.khalid@gmail.com>
2015-07-10 01:38:20 +0300

    Split tests into two separate files

--- pq_test.go
+++ pq_test.go
@@ -31,34 +31,3 @@ func TestMaxPQ(t *testing.T) {
 		t.Fatalf("%s shouldn't be on the top", job)
 	}
 }
-
-func TestMinPQ(t *testing.T) {
-	fmt.Println("Test Min PQ")
-	pq := NewMin()
-	if pq.Peek() != nil {
-		t.Fatal("Peek should return nil when empty")
-	}
-	items := []*Item{
-		{Value: 22, Priority: 200},
-		{Value: 52, Priority: 100},
-	}
-
-	for _, item := range items {
-		pq.Push(item.Value, item.Priority)
-	}
-
-	job := pq.Peek()
-	if job.Value != items[len(items)-1].Value {
-		t.Fatalf("Job value must equal %s", items[len(items)-1].Value)
-	}
-
-	pq.Update(job, job.Value, 250)
-
-	if job.Priority != 250 {
-		t.Fatalf("%s priority should be 250", job)
-	}
-
-	if j := pq.Peek(); j.Value == job.Value {
-		t.Fatalf("%s shouldn't be on the top", job)
-	}
-}
